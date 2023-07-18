56b74a43fb4f14658683c5d807e95c88b7350ac4
Nishant Racherla <nishant.racherla@gmail.com>
2023-07-17 20:07:00 -0700

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ClientOnly from "@/components/ClientOnly";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'NutriDiet',
-  description: 'The Perfect Source of Nutrition Facts for You',
-}
+  title: "NutriDiet",
+  description: "The Perfect Source of Nutrition Facts for You",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
