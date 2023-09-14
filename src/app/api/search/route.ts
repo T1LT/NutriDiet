c0a0ad0a34000cbc1f2c40058f1b10b6faa24e79
Nishant Racherla <nishant.racherla@gmail.com>
2023-09-14 02:39:16 -0700

    Add prisma call templates to search route

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: Request) {
   const formData = await req.formData();
-  const input = formData.get("input");
-  const inputType = formData.get("inputType");
+  const input = formData.get("input") as string;
+  const inputType = formData.get("inputType") as string;
 
   if (!input) {
     return NextResponse.json(
@@ -12,10 +14,22 @@ export async function POST(req: Request) {
     );
   }
 
-  // TODO: CALL TO PRISMA
-  // if (inputType === "Food Item") {
-  //    // make prisma call to match input
-  // } else {
-  //    // make prisma call to get column names
-  // }
+  if (inputType === "Food Item") {
+    // prisma call to search by input text
+    const result = await prisma.nutritionFacts.findMany({
+      where: {
+        name: {
+          contains: input,
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    // build response using result
+  } else {
+    // prisma call to get column names
+    // const cols = Prisma.dmmf.datamodel.models.map(model => [model.name, model.fields.map(a => a.name)]);
+    
+    // build response using cols
+  }
 }
