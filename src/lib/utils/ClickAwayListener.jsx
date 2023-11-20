73ce378e741b0b706a8b58d9d74236900485573f
Nishant Racherla <nishant.racherla@gmail.com>
2023-11-19 18:46:14 -0800

    create clickawaylistener wrapper component

---
+++ src/lib/utils/ClickAwayListener.jsx
@@ -0,0 +1,19 @@
+export default function ClickAwayListener(props) {
+  const { children, onClickAway } = props;
+
+  useEffect(() => {
+    function handleClick(event) {
+      if (!event.target.closest(".click-away-listener")) {
+        onClickAway();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [onClickAway]);
+
+  return <div className="click-away-listener">{children}</div>;
+}
