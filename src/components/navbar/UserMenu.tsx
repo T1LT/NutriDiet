9403b33f0128f831b1d2ecd011f6bd2f6dcc0175
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-08 17:32:50 -0800

    Fix close user dropdown bug

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from "@/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/types";
 import { useRouter } from "next/navigation";
+import ClickAwayListener from "@/lib/utils/ClickAwayListener";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -24,6 +25,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     setIsOpen((value) => !value);
   }, []);
 
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
   const handleLogout = () => {
     signOut();
     toggleOpen();
@@ -32,15 +37,17 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div
-          onClick={toggleOpen}
-          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block">
-            <Avatar />
+        <ClickAwayListener onClickAway={closeModal}>
+          <div
+            onClick={toggleOpen}
+            className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          >
+            <AiOutlineMenu />
+            <div className="hidden md:block">
+              <Avatar />
+            </div>
           </div>
-        </div>
+        </ClickAwayListener>
       </div>
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-[15vw] bg-white overflow-hidden right-0 top-12 text-sm">
--- src/lib/utils/ClickAwayListener.jsx
+++ src/lib/utils/ClickAwayListener.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function ClickAwayListener(props) {
   const { children, onClickAway } = props;
 
